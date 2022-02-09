b60cf9f21b50f144d6fce52b2341aca1a3b99ea2
AvzLucas <88989471+AvzLucas@users.noreply.github.com>
2022-02-09 14:04:17 -0300

    refactor of event listeners

--- controller/routing.js
+++ controller/routing.js
@@ -5,6 +5,7 @@ module.exports = function(io){
     let pomodoroCycle = 0;
     console.log(pomodoroCycle)
     let tm = new timer.Timer()
+    let interv = null
     
     io.on('connection', (socket)=>{
         console.log('front conectado')
@@ -16,39 +17,22 @@ module.exports = function(io){
         })
 
         socket.on('startTimer',()=> {
-            socket.offAny('pauseTimer',()=>{
-                tm.pause()
-            })
-            socket.offAny('resumeTimer',  ()=>{
-                tm.start()
-            })
+
             console.log('ouvi o evento')
             if(pomodoroCycle == 5){
                 tm.start({countdown: true, startValues : {seconds : 10}, targetValues : {seconds : 0}})
             }else{
                 tm.start({countdown: true, startValues : {seconds : 5}, targetValues : {seconds : 0}})
             }
-            // listen for pause and resume
-            socket.on('pauseTimer', ()=>{
-                tm.pause()
-            })
-
-            socket.on('resumeTimer', ()=>{
-                tm.start()
-            })
-
+            
             tm.addEventListener('targetAchieved',()=>{
                 console.log('target achieved')
 
                 socket.emit('pomodoroFinished')
 
-                let interv = setInterval(()=>{
+                interv = setInterval(()=>{
                     socket.emit('pomodoroFinished')
                 }, 10000)
-
-                socket.on('interaction',()=>{
-                    clearInterval(interv)
-                })
 
                 pomodoroCycle++
                 tm.removeAllEventListeners()
@@ -56,73 +40,44 @@ module.exports = function(io){
         })
 
         socket.on('postponeBreak', ()=>{
-            socket.offAny('pauseTimer',  ()=>{
-                tm.pause()
-            })
-            socket.offAny('resumeTimer', ()=>{
-                tm.start()
-            })
             console.log('adiar a pausa em 10min')
             tm.start({countdown: true, startValues : {seconds : 10}, targetValues : {seconds : 0}})
-
-            // listen for pause and resume
-            socket.on('pauseTimer', ()=>{
-                tm.pause()
-            })
-
-            socket.on('resumeTimer', ()=>{
-                tm.start()
-            })
-
 
             tm.addEventListener('targetAchieved', ()=>{
                 console.log('target achieved')
 
                 socket.emit('pomodoroFinished')
 
-                let interv = setInterval(()=>{
+                interv = setInterval(()=>{
                     socket.emit('pomodoroFinished')
                 }, 10000)
-
-                socket.on('interaction',()=>{
-                    clearInterval(interv)
-                })
 
                 tm.removeAllEventListeners()
             })
         })
 
         socket.on('break', ()=>{
-            socket.offAny('pauseTimer', ()=>{
-                tm.pause()
-            })
-            socket.offAny('resumeTimer', ()=>{
-                tm.start()
-            })
             console.log('iniciando uma pausa')
             tm.start({countdown: true, startValues : {seconds : 5}, targetValues : {seconds : 0}})     
-            // listen for pause and resume timer 
-            socket.on('pauseTimer', ()=>{
-                tm.pause()
-            })
-
-            socket.on('resumeTimer', ()=>{
-                tm.start()
-            })
-  
             tm.addEventListener('targetAchieved', ()=>{
                 socket.emit('breakIsOver')
-                let interv = setInterval(()=>{
+                interv = setInterval(()=>{
                     socket.emit('breakIsOver')
                 }, 10000)
-
-                socket.on('interaction',()=>{
-                    clearInterval(interv)
-                })
-
                 console.log('pausa encerrada')
                 tm.removeAllEventListeners()
             })
+        })
+        // listen for pause and resume
+        socket.on('pauseTimer', ()=>{
+            tm.pause()
+        })
+
+        socket.on('resumeTimer', ()=>{
+            tm.start()
+        })
+        socket.on('interaction',()=>{
+            clearInterval(interv)
         })
     })
 }
