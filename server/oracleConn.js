01cabfc6aa5bc42d19233249ad9b357ed0f458f7
AvzLucas <88989471+AvzLucas@users.noreply.github.com>
2022-02-14 08:30:12 -0300

    start develping features that require db use

---
+++ server/oracleConn.js
@@ -0,0 +1,15 @@
+var oracle = require('oracledb');
+
+oracle.initOracleClient({libDir: "C:/Program Files/instantclient_21_3"});
+var connOracle = async function(){
+    return await oracle.getConnection({
+    user : 'treinamento07',
+    password: 'treinamento07',
+    connectString: '192.168.20.239:1521/DESENVV4.kunden.local'
+})
+console.log('conexão estabelecida');}
+
+module.exports = async function(){
+    console.log('Módulo de conexão com o db foi carregado')
+    return await connOracle();
+}
