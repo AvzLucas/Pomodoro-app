b5af72d11bfa04e5db70c5d6e695152e6dc71077
AvzLucas <88989471+AvzLucas@users.noreply.github.com>
2022-04-07 09:37:08 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-    res.sendFile('C:/Users/lucas.avozani/Documents/GitHub/Pomodoro-app/public/views/index.html')
+//     res.sendFile('path to main view')
 })
 
 app.use(express.json())
